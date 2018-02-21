f9b35707dc198838370c12948256372d1d49dd1a
eugene <eugene.unn@gmail.com>
2018-02-21 12:48:51 +0300

    Update test1

--- test/test1.cpp
+++ test/test1.cpp
@@ -7,7 +7,15 @@ using grain::GrainMesh;
 int main(int argc, char** argv)
 {
 	GrainMesh gmsh;
+	
+	// Geerate stub tetrahedron 
 	gmsh.makeStubTetra();
+
+	// Check number of vertices and facets in tetrahedron
+	if (gmsh.getVerticesCount() != 4)
+		return 1;
+	if (gmsh.getTrianglesCount() != 4)
+		return 2;
 
     return 0;
 }
--- test/testcgal1.cpp
+++ test/testcgal1.cpp
@@ -1,4 +1,5 @@
 #include "common.h"
+#include "adaptorcgal.h"
 
 int main(int argc, char** argv)
 {
