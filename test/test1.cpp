2f48493161dd7a330d999f337e304f8e6c65e60b
eugene <eugene.unn@gmail.com>
2018-02-21 12:03:05 +0300

    Add tests

---
+++ test/test1.cpp
@@ -0,0 +1,13 @@
+#include "common.h"
+#include "grainmesh.h"
+#include "iotetgen.h"
+
+using grain::GrainMesh;
+
+int main(int argc, char** argv)
+{
+	//GrainMesh gmsh;
+	//gmsh.makeStubTetra();
+
+    return 0;
+}
